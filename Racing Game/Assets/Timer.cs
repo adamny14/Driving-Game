f549012bfe910cb1bf2fc28d760d9ffd39c4ecf7
Adam Hussain <adamahussain528@gmail.com>
2018-11-29 22:38:37 -0500

    Added collision with time increase, and collision objects

--- Racing Game/Assets/Timer.cs
+++ Racing Game/Assets/Timer.cs
@@ -9,20 +9,39 @@ public class Timer : MonoBehaviour {
     public int maxTime = 0; //Seconds Overall
     public Text countdown; //UI Text Object
     private int time = 0;
+    public GameObject s;
     void Start()
     {
         StartCoroutine("startTime");
         Time.timeScale = 1; //Just making sure that the timeScale is right
     }
 
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.CompareTag("Barrel"))
+        {
+            col.gameObject.SetActive(false);
+            Destroy(s);
+            time += 10;
+        }
+       
+
+    }
+
+    
+
     void Update()
     {
         if (time > maxTime)
             countdown.text = ("You have Lost");
         else
-            countdown.text = ("" + time); //Showing the Score on the Canvas
+        {
+            
+                countdown.text = ("" + time); //Showing the Score on the Canvas
+            
+        }
     }
-    //Simple Coroutine
+    //Simple Co-routine
     IEnumerator startTime()
     {
         while (true)
