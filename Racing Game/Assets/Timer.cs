358611e5b316a0a8967213afe57ae3f8526d4b51
Adam Hussain <adamahussain528@gmail.com>
2018-12-17 22:45:35 -0500

    Fixed timer and added CarObject

--- Racing Game/Assets/Timer.cs
+++ Racing Game/Assets/Timer.cs
@@ -32,16 +32,22 @@ public class Timer : MonoBehaviour {
             //Destroy(s);
             if (time < maxTime - 30)
             {
-                countdown.text = ("You Won - Your Time: " + time + " is less than the target time!");
+                countdown.text = ("You Won - You Beat the target time");
                 gameOver = true;
             }
             else if (time > maxTime - 30)
             {
-                countdown.text = ("You Lost - Your Time: " + time + " is greater than the target time!");
+                countdown.text = ("You Lost - You took longer than the target time!");
                 gameOver = true;
             }
         }
 
+        if (col.gameObject.CompareTag("CarObject"))
+        {
+            col.gameObject.SetActive(false);
+            Destroy(s);
+            time += 20;
+        }
     }
 
     
@@ -49,8 +55,36 @@ public class Timer : MonoBehaviour {
     void Update()
     {
         if (gameOver == false)
-        {  
-                countdown.text = ("" + time); //Showing the Score on the Canvas            
+        {
+            
+                if(time < 60)
+                    countdown.text = ("" + time); //Showing the Score on the Canvas     
+                 else if (time > 60 && time < 120)
+                    if(time < 70)
+                        countdown.text = ("1:0" + (time-60));      
+                    else
+                        countdown.text = ("1:" + (time - 60));
+                else if (time > 120 && time < 180)
+                    if (time < 130)
+                        countdown.text = ("2:0" + (time - 120));
+                    else
+                        countdown.text = ("2:" + (time - 120));
+                else if (time > 180 && time < 240)
+                    if (time < 190)
+                        countdown.text = ("3:0" + (time - 180));
+                    else
+                        countdown.text = ("3:" + (time - 180));
+                else if (time > 240 && time < 300)
+                    if (time < 250)
+                        countdown.text = ("4:0" + (time - 240));
+                    else
+                        countdown.text = ("4:" + (time - 240));
+                else if (time > 300 && time < 360)
+                    if (time < 310)
+                        countdown.text = ("5:0" + (time - 300));
+                    else
+                        countdown.text = ("5:" + (time - 300));
+
         }
     }
     //Simple Co-routine
